0badd1ded9963c1487ef6d0e079d2b9eae3df49c
Ugboaja-Uchechi <stephanieuchechi754@gmail.com>
2024-06-11 01:33:15 +0100

    Add canvas sketch util and draw repetitive circles

--- circles.js
+++ circles.js
@@ -1,4 +1,6 @@
 const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: [ 2048, 2048 ]
@@ -6,8 +8,58 @@ const settings = {
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
+    context.fillStyle = 'lavender';
+    context.strokeStyle = 'crimson';
+
+    const cx = width * 0.5;
+    const cy = height * 0.5;
+
+    
+    // 30% of the canvas width and height is used
+    const w = width * 0.01;
+    const h = height * 0.1;
+
+    let x, y;
+
+    let num = 180;
+    const radius = width * 0.3;
+
+    for (let i =0; i < num; i++) {
+      // change the angle of the location by the index of the loop
+      const slice = math.degToRad(50 + num);
+      const angle = slice * i;
+
+      x = cx + radius * Math.sin(angle);
+      y = cy + radius * Math.cos(angle);
+
+      context.save();
+      context.translate(x, y)
+      context.rotate(-angle);
+
+      // scale takes 2 parameters and the default value is 1
+      // a random value from 1 - 3. math.random's default starts from 0
+      context.scale(random.range(1, 2), random.range(2, 1.5))
+
+      context.beginPath();
+      context.rect(-w * 0.2, random.range(0, -h * 0.2), w, h );
+      context.fill();
+      context.restore();
+
+      context.save();
+      context.translate(cx, cy);
+      context.rotate(angle );
+
+      context.lineWidth = random.range(4, 6);
+
+      context.beginPath();
+      context.arc(0, 0, radius * random.range(0.2 /4, 3), slice * random.range(1, -8), slice * random.range(1,8));
+
+      context.stroke();
+
+      context.restore();
+    }
   };
 };
 
